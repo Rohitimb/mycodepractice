00a741861d252effac2a8263b7206e78024fb780
Rohit Dharaviya <41590389+Rohitimb@users.noreply.github.com>
2018-11-22 12:29:34 +0530

    Add files via upload

---
+++ cpp/capturelist_lambda.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int one = 1;
+	int two = 2;
+	int three = 3;
+
+	// Capture one and two by value
+	[one, two](){ cout << one << ", " << two << endl; }();
+
+	// Capture all local variables by value.
+	[=](){ cout << one << ", " << two << endl; }();
+
+	// Default capture all local variables by value, but three by reference.
+	[=, &three](){ three = 7; cout << one << ", " << two << endl; }();
+	cout << three << endl;
+
+	// Default capture all local variables by reference.
+	[&](){ three = 7; two = 8; cout << one << ", " << two << endl; }();
+	cout << two << endl;
+
+	// Default capture all local variables by reference but two and three by value.
+	[&, two, three](){ one = 100;  cout << one << ", " << two << endl; }();
+	cout << one << endl;
+
+	return 0;
+}
