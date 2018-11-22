fb4f163ca01276c8a58bf5f9e682b8ffe689464d
Rohitimb <rohit.eceng@gmail.com>
2018-11-22 15:26:40 +0530

    pipe in parent child

--- pipe/two_pipe_parent_child.c
+++ pipe/two_pipe_parent_child.c
@@ -2,14 +2,13 @@
 #include<stdlib.h>
 #include<string.h>
 #include<unistd.h>
+#include<ctype.h>
 
 int main()
 {
-	int p[2],q[2];
-	char msgbufsnd1[24] = "msg pipe 1";
-	char msgbufsnd2[24] = "msg pipe 2";
-	char msgbufrcv1[24] = "";
-	char msgbufrcv2[24] = "";
+	int p[2],q[2],i;
+	char msgbufsnd[24] = "msg to be send";
+	char msgbufrcv[24] = "";
 	
 	if(pipe(p) < 0)
 	{
@@ -28,14 +27,19 @@ int main()
 		close(p[1]);
 		close(q[0]);
 
-		if(read(p[0],msgbufrcv1,sizeof(msgbufrcv1)) < 0)
+		if(read(p[0],msgbufrcv,sizeof(msgbufrcv)) < 0)
 		{
 			perror("read");
 			return (EXIT_FAILURE);
 		}
-		printf("Received from pipe 1 = %s\n",msgbufrcv1);
-		printf("sent from pipe 2 = %s\n",msgbufsnd2);
-		if(write(q[1],msgbufsnd2,strlen(msgbufsnd2) + 1) < 0)
+		printf("Received from pipe 1 = %s\n",msgbufrcv);
+		for(i=0;msgbufrcv[i];i++)
+		{
+			if(islower(msgbufrcv[i]))
+				msgbufrcv[i] = toupper(msgbufrcv[i]);
+		}
+		printf("sent from pipe 2 = %s\n",msgbufrcv);
+		if(write(q[1],msgbufrcv,strlen(msgbufrcv) + 1) < 0)
 		{
 			perror("write");
 			return (EXIT_FAILURE);
@@ -46,19 +50,19 @@ int main()
 	{//parent
 		close(p[0]);
 		close(q[1]);
-		printf("sent from pipe 1 = %s\n",msgbufsnd1);
-		if(write(p[1],msgbufsnd1,strlen(msgbufsnd1) + 1) < 0)
+		printf("sent from pipe 1 = %s\n",msgbufsnd);
+		if(write(p[1],msgbufsnd,strlen(msgbufsnd) + 1) < 0)
 		{
 			perror("write");
 			return (EXIT_FAILURE);
 		}
 
-		if(read(q[0],msgbufrcv2,sizeof(msgbufrcv2)) < 0)
+		if(read(q[0],msgbufrcv,sizeof(msgbufrcv)) < 0)
 		{
 			perror("read");
 			return (EXIT_FAILURE);
 		}
-		printf("Received from pipe 2 = %s\n",msgbufrcv2);
+		printf("Received from pipe 2 = %s\n",msgbufrcv);
 	}
 	return 0;
 }
