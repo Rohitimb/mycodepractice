54a5aa31a65bc45f57d496f5f4aa63f59a7a4347
Rohitimb <rohit.eceng@gmail.com>
2018-12-06 15:08:40 +0530

    shorthand shifting operators added

---
+++ operator overloading/friend member both/shorthand/left_shift_equal.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+//#define MEMBER
+using namespace std;
+
+class A
+{
+	private:
+		int x,y;
+	public:
+		A();
+		A(int,int);
+		~A();
+		void print();
+		#ifdef MEMBER
+			A operator <<= (const A&);
+		#else
+			friend A operator <<= (A&,const A&);
+		#endif
+};
+
+A :: A():x(10),y(20)
+{
+		cout << "default constructor" << endl;
+}
+A :: A(int i,int j):x(i),y(j)
+{
+		cout << "parameterized constructor" << endl;
+}
+A :: ~A()
+{
+		cout << "destructor" << endl;
+}
+
+void A :: print()
+{
+		cout << "x = " << x << " y = " << y << endl;
+}
+
+#ifdef MEMBER
+A A ::  operator <<= (const A& ob)
+{
+	x <<= ob.x;
+	y <<= ob.y;
+	return *this;
+}
+#else
+A operator <<= (A& ob1,const A& ob2)
+{	
+	ob1.x <<= ob2.x;
+	ob1.y <<= ob2.y;
+	return ob1;
+}
+#endif
+int main()
+{
+	A ob1(30,20);	
+	A ob2(2,3);
+	
+	ob1.print();
+
+	ob1 <<= ob2;
+
+	ob1.print();
+	ob2.print();
+	
+	return 0;
+}
