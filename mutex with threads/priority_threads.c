3ec28620d897716bfa93a13a61ad25946a13b59d
Rohitimb <rohit.eceng@gmail.com>
2018-12-14 18:16:24 +0530

    priority thread changes added

--- mutex with threads/priority_threads.c
+++ mutex with threads/priority_threads.c
@@ -1,162 +1,128 @@
-#include<stdio.h>
-#include<pthread.h>
-#include<unistd.h>
-#include<sched.h>
-#include <errno.h>
+#include<stdio.h>      
 #include<stdlib.h>
+#include<pthread.h>  
+#include<sched.h>     
 
-void *thread_fun(void*);
+void *thread_fun(void *);
 
-struct sched_param sch;
-pthread_attr_t att;
+#define MAX 5
+#define CHANGE 2
 
 int main()
 {
-	int i;
-	int policy;
+	pthread_t var[MAX],main_id;
+	pthread_attr_t attr[MAX];
 	
-	pthread_t v[5];
+	struct sched_param param;
+	
+	int policy=SCHED_FIFO;
+	int min,max;
 
-	printf("Thread 3 is having highest priority\n");
+	unsigned long mask = 1; /* use only 1 CPU core */
+	unsigned int len = sizeof(mask);
 	
-	if(0 != pthread_attr_init(&att))
+	if(0 != sched_setaffinity(0, len, &mask))
 	{
-		perror("pthread_attr_init");
+		perror("sched_setaffinity");
 		return 1;
 	}
-
-    pthread_getschedparam(v[2],&policy, &sch);
-	perror("thread_getschedparam");
-	printf("policy = %d priority = %d\n",policy,sch.sched_priority);
-
-	policy = SCHED_RR;
-	sch.sched_priority = 25;
-	pthread_setschedparam(v[2],policy,&sch);
-	perror("thread_getschedparam");
-	printf("policy = %d priority = %d\n",policy,sch.sched_priority);
-
-    pthread_getschedparam(v[2],&policy, &sch);
-	perror("thread_getschedparam");
-	printf("policy = %d priority = %d\n",policy,sch.sched_priority);
-	
-	#if 0
-   	if(0 != pthread_attr_getschedparam((const pthread_attr_t *)&att,&policy))
-	{	
-		perror("pthread_attr_getschedparam");
-		return 1;
-	}
-	printf("policy = %d\n",policy);
-	
-	policy = SCHED_RR;
-	
-   	if(0 != pthread_attr_setschedpolicy(&att,policy))
-	{	
-		perror("pthread_attr_setschedparam");
-		return 1;
-	}
-	printf("policy = %d\n",policy);
-   	
-   	if(0 != pthread_attr_getschedparam((const pthread_attr_t *)&att,&policy))
-	{	
-		perror("pthread_attr_getschedparam");
-		return 1;
-	}
-	printf("policy = %d\n",policy);
-
-	
-	#if 1
-	if(0 != pthread_attr_getschedparam(&att,&sch))
+	if(0 != sched_getaffinity(0, len, &mask))
 	{
-		perror("pthread_attr_getschedparam");
+		perror("sched_getaffinity");
 		return 1;
-	}
-	printf("\nScheduling Priority = %d\n",sch.sched_priority);
-	(sch.sched_priority) = 25;
-
-	int retval = 0;	
-	if( (retval = pthread_attr_setschedparam(&att,(const struct sched_param*)&sch)) )
-	{
-		perror("pthread_attr_setschedparam");
-		printf("Error no: %d retval (%d) \n",errno, retval);
-		//return 1;
 	}
 	
+	max = sched_get_priority_max(SCHED_FIFO);
+	min = sched_get_priority_min(SCHED_FIFO);
 
-	if(0 != pthread_attr_getschedparam(&att,&sch))
+	main_id = pthread_self();
+	param.sched_priority = max;
+	
+	if(0 != pthread_setschedparam(main_id, policy, &param))
 	{
-		perror("pthread_attr_getschedparam");
+		perror("pthread_setschedparam");
 		return 1;
 	}
-	printf("\nScheduling Priority = %d\n",sch.sched_priority);
-	#endif
-	#endif
-	for(i=0;i<5;i++)
+	
+	for(int  i=0;i<MAX;i++)
 	{
-		if(i == 2)
+		if(i == CHANGE)
 		{
-			int *param = (int*)malloc(sizeof(int));
-			*param = i;
-	
-			policy = SCHED_RR;
-			sch.sched_priority = 25;
-			pthread_setschedparam(v[i],policy,&sch);
-	
-			if(0 != pthread_create(&v[i],&att,thread_fun,(void*)param))
-			{
-				perror("pthread_create");
-				return 1;
-			}
+			/*changing the priority*/
+			param.sched_priority = 30;
 		}
-		else
+	
+		if(0 != pthread_attr_init(&attr[i]))
 		{
-			int *param2 = (int*)malloc(sizeof(int));
-			*param2 = i;
-			if(0 != pthread_create(&v[i],NULL,thread_fun,(void*)param2))
-			{
-				perror("pthread_create");
-				return 1;
-			}
+			perror("pthread_attr_init");
+			return 1;
 		}
-    	pthread_getschedparam(v[i],&policy, &sch);
-		printf("thread = %d policy = %d priority = %d\n",i,policy,sch.sched_priority);
-	}
-	
-	
-	for(i=0;i<5;i++)
-	{
-		if(0 != pthread_join(v[i],NULL))
+
+		if(0 != pthread_attr_setschedpolicy(&attr[i],policy))
 		{
-			perror("pthread_join");
+			perror("pthread_attr_setschedpolicy");
+			return 1;
+		}
+
+		if(0 != pthread_attr_setschedparam(&attr[i],&param))
+		{
+			perror("pthread_attr_setschedparam");
+			return 1;
+		}
+	
+		int *p1 = (int*)malloc(sizeof(int));
+		*p1 = i;
+
+		if(0 != pthread_create(&var[i], &attr[i], thread_fun,p1))
+		{
+			perror("pthread_create");
+			return 1;
+		}
+		if(0 != pthread_setschedparam(var[i],policy,&param))
+		{
+			perror("pthread_setschedparam");
+			return 1;
+		}
+	}
+	for(int i=0;i<MAX;i++)
+	{
+		if(0 != pthread_join(var[i], NULL))
+		{
+			perror("pthread_join(var, NULL)");
+			return 1;
+		}
+	}
+		
+	for(int i=0;i<MAX;i++)
+	{
+		if(0 != pthread_attr_destroy(&attr[i]))
+		{
+			perror("pthread_attr_destroy");
 			return 1;
 		}
 	}
 	
-	if(0 != pthread_attr_destroy(&att))
-	{
-		perror("pthread_attr_destroy");
-		return 1;
-	}
 	pthread_exit(NULL);
-	
-	printf("Done, in main function now\n");
-	
-	return 0;
+	return 0;		
 }
 
-void* thread_fun(void* p)
+void *thread_fun(void *ptr)
 {
-	int *var = (int*)p;
-	int i;
-//	printf("Start work thread %d \n",*var);
-	
-	for(i=0;i<200000000;i++)
+	int j,i;
+	int *p = (int*)ptr;
+	printf("Thread start = %d\n",*p);
+
+	for(i=0;i<2;i++)
 	{
-		//sleep(1);
+		for(j=1;j<=100000000;j++)
+		{
+			//do operation just going through the loop
+		}
 	}
-	
-	printf("End work thread %d \n",*var);
-	free(var);
-	
+	printf("Thread end = %d\n",*p);
+
 	pthread_exit(NULL);
+	
+	return NULL;
 }
 
